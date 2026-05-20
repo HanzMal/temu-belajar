bcdaf80a682a296d3fa452e8ed111d0bd1bd90be
Muhammad Farhan <muhammadfarhan.2017@student.uny.ac.id>
2026-05-20 14:55:16 +0700

    inisialisasi sequalize dan database

--- index.js
+++ index.js
@@ -2,35 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// 1. Import library pg (ambil Pool nya)
 const { Pool } = require('pg')
+require("dotenv").config()
+const {Sequelize, DataTypes} = require('sequelize')
 
-// 2. Setup koneksi (Pool)
-// Note: User default postgres biasanya 'postgres' bukan 'root', dan butuh password.
-const pool = new Pool({
-    user: 'postgres',           // User database (dari screenshot)
-    host: 'localhost',          // Host (dari screenshot)
-    database: 'temuevent-master', // Database yang ingin digunakan
-    password: 'postgres',  // Password yang tersimpan di DBeaver
-    port: 5444,                 // Port dari screenshot (bukan 5432!)
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    logging: console.log,
+});
+
+sequelize.authenticate()
+.then(() => {
+    console.log("Database Connected"); 
 })
-
-// 3. Cek Koneksi
-// Pool menggunakan method connect(callback) atau query
-pool.connect((err, client, release) => {
-    if (err) {
-        // Jika error
-        console.log("Database gagal terhubung bang", err)
-        return
-    }
-
-    // Jika sukses
-    console.log("Connected to PostgreSQL!")
-
-    // Selalu lepas koneksi (release) kalau pakai method connect
-    release()
-})
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
+.catch((err) => {
+    console.log("Failed Connect", err)
 })
